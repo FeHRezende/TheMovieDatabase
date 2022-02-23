014a1cd0c0440520da3608893137d790bad7ec38
Fernanda Rezende <32329103+FeHRezende@users.noreply.github.com>
2022-02-22 21:29:01 -0300

    Teste :)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,25 +1,26 @@
 import axios from 'axios';
-const API_KEY = '1fa853000dd334b4115f711b80abea48';
 
-const getPopularMovies = async (page) => await axios.get(`https://api.themoviedb.org/3/movie/popular?&api_key=${API_KEY}&page=${page}&language=pt-BR`)
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const getPopularMovies = async (page) => await axios.get(`https://api.themoviedb.org/3/movie/popular?&api_key=${apiKey}&page=${page}&language=pt-BR`)
   .then((response) => response.data)
 
-const getMovie = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=pt-BR`)
+const getMovie = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
-const getTrailers = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=pt-BR`)
+const getTrailers = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
-const getRecommendations = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}&language=pt-BR`)
+const getRecommendations = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
-const getCredits = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language=pt-BR`)
+const getCredits = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
-const getReleaseDates = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${API_KEY}&language=pt-BR`)
+const getReleaseDates = async (id) => await axios.get(`https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
-const getGenres = async () => await axios.get(`https://api.themoviedb.org/3/genre/movie/list?&api_key=${API_KEY}&language=pt-BR`)
+const getGenres = async () => await axios.get(`https://api.themoviedb.org/3/genre/movie/list?&api_key=${apiKey}&language=pt-BR`)
   .then((response) => response.data)
 
 export { getPopularMovies, getMovie, getTrailers, getRecommendations, getCredits, getReleaseDates, getGenres };
